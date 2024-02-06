3ac13072f6b0b4cd93542db56a937784c30376af
asiddiqa249 <asiddiqa249@gmail.com>
2024-02-06 22:52:56 +0530

    loading updated

--- src/Component/WeatherApp/WeatherApp.js
+++ src/Component/WeatherApp/WeatherApp.js
@@ -13,59 +13,65 @@ const WeatherApp = () => {
   let key = "d114c7997eeb3530f553fbe64a999a09";
   
     const [wicon, setWicon] = useState(cloud_icon);
+    const [loading, setLoading] = useState(false);
   const search = async() => {
     const element = document.getElementsByClassName("cityInput");
     if (element[0].value === "") {
       return 0;
     }
-    let url = `https://api.openweathermap.org/data/2.5/weather?q=${element[0].value}&units=Metric&appid=${key}`;
-    let response= await fetch(url);
-    let data = await response.json();
-    const humidity = document.getElementsByClassName("humidity-percent");
-    const wind=document.getElementsByClassName("wind-rate")
-    const temp = document.getElementsByClassName("weather-temp");
-    const location = document.getElementsByClassName("weather-location");
+    setLoading(true);
+      try {
+          let url = `https://api.openweathermap.org/data/2.5/weather?q=${element[0].value}&units=Metric&appid=${key}`;
+          let response = await fetch(url);
+          let data = await response.json();
+          const humidity = document.getElementsByClassName("humidity-percent");
+          const wind = document.getElementsByClassName("wind-rate")
+          const temp = document.getElementsByClassName("weather-temp");
+          const location = document.getElementsByClassName("weather-location");
     
-    humidity[0].innerHTML = `${data.main.humidity} %`;
-    wind[0].innerHTML = `${Math.floor(data.wind.speed)} km/h`;
-    temp[0].innerHTML = `${Math.floor(data.main.temp)}℃`;
-    location[0].innerHTML = data.name;
+          humidity[0].innerHTML = `${data.main.humidity} %`;
+          wind[0].innerHTML = `${Math.floor(data.wind.speed)} km/h`;
+          temp[0].innerHTML = `${Math.floor(data.main.temp)}℃`;
+          location[0].innerHTML = data.name;
     
-    if (data.weather[0].icon === "01d" || data.weather[0].icon === "01n") {
-      setWicon(clear_icon);
-    } else if (
-      data.weather[0].icon === "02d" ||
-      data.weather[0].icon === "02n"
-    ) {
-      setWicon(cloud_icon);
-    } else if (
-      data.weather[0].icon === "03d" ||
-      data.weather[0].icon === "03n"
-    ) {
-      setWicon(drizzle_icon);
-    } else if (
-      data.weather[0].icon === "04d" ||
-      data.weather[0].icon === "04n"
-    ) {
-      setWicon(drizzle_icon);
-    } else if (
-      data.weather[0].icon === "09d" ||
-      data.weather[0].icon === "09n"
-    ) {
-      setWicon(rain_icon);
-    } else if (
-      data.weather[0].icon === "10d" ||
-      data.weather[0].icon === "10n"
-    ) {
-      setWicon(rain_icon);
-    } else if (
-      data.weather[0].icon === "13d" ||
-      data.weather[0].icon === "13n"
-    ) {
-      setWicon(snow_icon);
-    } else {
-        setWicon(clear_icon)
-    }
+          if (data.weather[0].icon === "01d" || data.weather[0].icon === "01n") {
+              setWicon(clear_icon);
+          } else if (
+              data.weather[0].icon === "02d" ||
+              data.weather[0].icon === "02n"
+          ) {
+              setWicon(cloud_icon);
+          } else if (
+              data.weather[0].icon === "03d" ||
+              data.weather[0].icon === "03n"
+          ) {
+              setWicon(drizzle_icon);
+          } else if (
+              data.weather[0].icon === "04d" ||
+              data.weather[0].icon === "04n"
+          ) {
+              setWicon(drizzle_icon);
+          } else if (
+              data.weather[0].icon === "09d" ||
+              data.weather[0].icon === "09n"
+          ) {
+              setWicon(rain_icon);
+          } else if (
+              data.weather[0].icon === "10d" ||
+              data.weather[0].icon === "10n"
+          ) {
+              setWicon(rain_icon);
+          } else if (
+              data.weather[0].icon === "13d" ||
+              data.weather[0].icon === "13n"
+          ) {
+              setWicon(snow_icon);
+          } else {
+              setWicon(clear_icon)
+          }
+      } finally {
+          setLoading(false);
+      }
   };
   return (
     <>
@@ -102,6 +108,7 @@ const WeatherApp = () => {
             </div>
           </div>
         </div>
+        {loading && <div className="loading-indicator">Loading...</div>}
       </div>
     </>
   );
